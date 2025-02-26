d5ec2d2f14d117d94d2f9ef3fb57d4f97a12525f
The-Obstacle-Is-The-Way <jj@novamindnyc.com>
2025-02-25 21:46:50 -0500

    auth endpoint

--- app/infrastructure/database/migrations/versions/09c26554662a_add_is_deleted_column_to_cravings.py
+++ app/infrastructure/database/migrations/versions/09c26554662a_add_is_deleted_column_to_cravings.py
@@ -1,27 +1,22 @@
-"""Add is_deleted column to cravings
+"""
+Add is_deleted column to cravings
 
 Revision ID: 09c26554662a
 Revises: 200c7d532370
 Create Date: 2025-02-25 16:53:36.946176
-
 """
-from typing import Sequence, Union
 
+from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-# revision identifiers, used by Alembic.
-revision: str = '09c26554662a'
-down_revision: Union[str, None] = '200c7d532370'
+revision: str = "09c26554662a"
+down_revision: Union[str, None] = "200c7d532370"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
-
 def upgrade() -> None:
-    # Add the is_deleted column to the cravings table.
-    op.add_column('cravings', sa.Column('is_deleted', sa.Boolean(), server_default='false', nullable=False))
-
+    op.add_column("cravings", sa.Column("is_deleted", sa.Boolean(), server_default="false", nullable=False))
 
 def downgrade() -> None:
-    # Drop the is_deleted column from the cravings table.
-    op.drop_column('cravings', 'is_deleted')
+    op.drop_column("cravings", "is_deleted")
